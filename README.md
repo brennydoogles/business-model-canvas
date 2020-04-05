e66ddce6c377eade3a02f41f3196739e8b8c0939
Brendon Dugan <brendondugan@gmail.com>
2020-04-05 17:06:36 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# business-model-canvas
+A simple HTML template for building a Business model canvas by only editing JSON
+Copy json/sample.json for each canvas you want to complete, and then add an entry for each in canvases.json to be able
+to easily swap back and forth between them.
+
+Simples!
---
+++ js/canvas.js
@@ -0,0 +1,74 @@
+
+
+$(document).ready(function () {
+    $("#dateField").text(moment().format("MMMM Do YYYY"));
+    createCanvasList();
+    $("#fileSelect").change(function(){
+        let selectedOption = $(this).children("option:selected");
+        let fileNameOfSelectedOption = selectedOption.attr("data-fileName");
+        clearCanvas();
+        fillInCanvas(fileNameOfSelectedOption);
+    });
+    fillInCanvas();
+});
+
+function createCanvasList() {
+    $.getJSON("../json/canvases.json", function (json) {
+        // console.log(json); // this will show the info it in firebug console
+        $.each(json.canvases, function (key, value) {
+            let option = $('<option></option>')
+                .attr("data-fileName", value.fileName)
+                .text(value.name);
+            $("#fileSelect").append(option);
+        });
+    });
+}
+
+function clearCanvas(){
+    $("#partnerList").empty();
+    $("#activityList").empty();
+    $("#resourceList").empty();
+    $("#valuePropositionList").empty();
+    $("#customerRelationshipList").empty();
+    $("#channelList").empty();
+    $("#customerSegmentList").empty();
+    $("#costStructureList").empty();
+    $("#revenueStreamList").empty();
+}
+
+function fillInCanvas(filename) {
+    if(filename === undefined){
+        filename = "sample.json";
+    }
+    $.getJSON("../json/" + filename, function (json) {
+        $("#createdByField").text(json.createdBy);
+        $.each(json.keyPartners, function (key, value) {
+            $("#partnerList").append("<li>" + value + "</li>")
+        });
+        $.each(json.keyActivities, function (key, value) {
+            $("#activityList").append("<li>" + value + "</li>")
+        });
+
+        $.each(json.keyResources, function (key, value) {
+            $("#resourceList").append("<li>" + value + "</li>")
+        });
+        $.each(json.valuePropositions, function (key, value) {
+            $("#valuePropositionList").append("<li>" + value + "</li>")
+        });
+        $.each(json.customerRelationships, function (key, value) {
+            $("#customerRelationshipList").append("<li>" + value + "</li>")
+        });
+        $.each(json.channels, function (key, value) {
+            $("#channelList").append("<li>" + value + "</li>")
+        });
+        $.each(json.customerSegments, function (key, value) {
+            $("#customerSegmentList").append("<li>" + value + "</li>")
+        });
+        $.each(json.costStructure, function (key, value) {
+            $("#costStructureList").append("<li>" + value + "</li>")
+        });
+        $.each(json.revenueStreams, function (key, value) {
+            $("#revenueStreamList").append("<li>" + value + "</li>")
+        });
+    });
+}
